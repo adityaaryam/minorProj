0df3dceb032213e7a47775ed9bf4579f4c463b14
adityaaryam <adityaaryam@gmail.com>
2023-12-08 01:34:46 +0530

    ec2

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const socket = require("socket.io");
-const sharp=require('sharp');
 const multer = require('multer');
 var fs=require('fs');
 const FormData = require('form-data');
